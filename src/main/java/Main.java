d02a97097b5cbb3e66bf6b456d9b4c1a69ec1573
ranma15g <ranma15g@gmail.com>
2024-08-26 00:21:02 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+/*
+
+Class: COP 2800: Java Programming
+Instructor: Francis Fiskey
+Description: 1.1 (Display three messages) Write a program that displays "Welcome to Java", "Welcome to Computer Science", and "Programming is fun". 
+Due: Sep 8 by 11:59pm
+I pledge by honor that I have completed the programming assignment independently.
+I have not copied the code from a student or any source.
+I have not given my code to any student.
+Sign here: ___Eric Snyder___
+
+*/
+
+// We define the public class Main to start the program
+public class Main { // This brace opens our class block
+
+  // We introduce the main method to run the program 
+  public static void main(String[] args) {
+        // This brace opens our main method block
+
+        // We print our first line
+        System.out.println("Welcome to Java");
+
+        // We print our second line
+        System.out.println("Welcome to Computer Science");
+
+        // We print our third line
+        System.out.println("Programming is fun");
+
+    } // This brace closes the main method block
+
+} // This brace closes the class block
+
+// And that's all folks!
